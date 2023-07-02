56062715a0966c6534e5840af770f8b5ad783aca
RuslanMirasov <info@mirasov.dev>
2023-07-02 23:58:49 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(
+	({ preview, original, description }) =>
+		` 
+   <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+         />
+      </a>
+   </li>
+   `
+);
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup.join(''));
+
+galleryEl.addEventListener('click', galleryModalOpen);
+
+function galleryModalOpen(e) {
+	e.preventDefault();
+	if (e.target.nodeName !== 'IMG') return;
+	basicLightbox.create(`<img width="1400" height="900" src="${e.target.dataset.source}">`).show();
+}
